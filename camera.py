348e42d7903bdfbd0d6e679a6af05aabe13e0f5e
gaelgil <gaelgilbello27@gmail.com>
2024-04-20 13:07:30 -0700

    added live predictipns

--- camera.py
+++ camera.py
@@ -54,7 +54,7 @@ class VideoCamera(object):
     def predict(self, frame):
         # Preprocess the frame
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        resized_frame = cv2.resize(gray_frame, (32, 32))  # Resize to match the expected input size
+        resized_frame = cv2.resize(gray_frame, (224, 224))  # Resize to match the expected input size
         tensor_frame = torch.tensor(resized_frame, dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(self.device)
 
         # Move model parameters to the same device as the input tensor
@@ -62,38 +62,21 @@ class VideoCamera(object):
 
         # Pass the frame through the model's Conv1 layer
         with torch.no_grad():
-            output = self.model.Conv1(tensor_frame)
-
-        return output
+            # output = self.model.Conv1(tensor_frame)
+            outputs=self.model(tensor_frame.to("cuda"))
+        
+        return outputs
     
     def get_frame(self):
         # extracting frames
         (rval, im) = self.video.read()
 
         predictions = self.predict(im)
-        print(predictions)
-        # im = cv2.flip(im, 1, 1)
-        # mini = cv2.resize(im, (im.shape[1] // size, im.shape[0] // size))
-        # faces = classifier.detectMultiScale(mini)
-        # for f in faces:
-        #     (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
-        #     #Save just the rectangle faces in SubRecFaces
-        #     face_img = im[y:y+h, x:x+w]
-        #     resized=cv2.resize(face_img,(300,300))
-        #     normalized=resized/255.0
-        #     reshaped=np.reshape(normalized,(1,300,300,3))
-        #     reshaped = np.vstack([reshaped])
-        #     K.set_session(session1)
-        #     with graph1.as_default():
-        #         results=loaded_model.predict(reshaped)
-        #     if results >.5:
-        #         result = np.array([[1]])
-        #     else:
-        #         result = np.array([[0]])
-        #     label = np.argmax(result)
-        #     cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[result[label][0]],2)
-        #     cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[result[label][0]],-1)
-        #     cv2.putText(im, labels_dict[result[label][0]], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
-        #     # encode OpenCV raw frame to jpg and displaying it
+        # print(predictions)
+        predicted = torch.softmax(predictions,dim=1)
+        _,predicted = torch.max(predicted, 1)
+   
+        print(list(predicted.data.cpu().numpy()))
+
         ret, jpeg = cv2.imencode('.jpg', im)
         return jpeg.tobytes()
