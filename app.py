d99370b2d3aa62c70ca0af48964d5fe99a3f7012
ehcaw <ryannguyenc@gmail.com>
2024-04-29 12:30:48 -0700

    working localhost implementation, with flask server that displays the predicted output

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from camera import VideoCamera
 from flask import Flask, render_template, Response
 import cv2
+import time
 
 app = Flask(__name__)
 
@@ -11,13 +12,21 @@ def gen_frames(camera):
     """
     function to get frames
     """
+    i = 0
     while True:
         #get camera frame
         frame, label = camera.get_frame()
-        print(label)
-        #print(frame)
+        if i % 100 == 0: 
+            print(label)
+        i += 1
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        #return label
+def gen_labels(camera):
+    while True:
+        label = camera.get_label()
+        print('label generated')
+        return label
 
 @app.route('/')
 def index():
@@ -28,6 +37,9 @@ def index():
 def video_feed():
     return Response(gen_frames(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/get_label')
+def get_label():
+    return Response(gen_labels(VideoCamera()), mimetype='text/plain')
 
 @app.route('/about')
 def hello():
--- camera.py
+++ camera.py
@@ -7,7 +7,8 @@ ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
 class VideoCamera(object):
     def __init__(self):
         self.model = SignLanguageCNN()
-        checkpoint = torch.load("./MLModel/Model_test.pt")
+        #checkpoint = torch.load("./MLModel/Model_test.pt")
+        checkpoint = torch.load("./MLModel/Model_test.pt", map_location=torch.device('cpu'))
         self.model.load_state_dict(checkpoint["state_dict"])
         self.model.eval()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,7 +29,7 @@ class VideoCamera(object):
         self.model.to(self.device)
 
         with torch.no_grad():
-            output=self.model(tensor_frame.to("cuda"))
+            output=self.model(tensor_frame.to("cuda") if torch.cuda.is_available() else tensor_frame.to("cpu"))
         
         return output
     
@@ -37,7 +38,8 @@ class VideoCamera(object):
         (rval, im) = self.video.read()
         im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  # grayscale
         # crop down to a 224x224 square in the center of the webcam
-        cv2.rectangle(im, (208, 128), (432, 352), (0, 0, 255), 2)
+        #cv2.rectangle(im, (208, 128), (432, 352), (0, 0, 255), 2)
+        cv2.rectangle(im, (208, 128), (656, 576), (0, 0, 255), 2)
         cropped = im[208:432, 128:352]
         predictions = self.predict(cropped)
         # print(predictions)
@@ -48,3 +50,19 @@ class VideoCamera(object):
 
         ret, jpeg = cv2.imencode('.jpg', im)
         return jpeg.tobytes(), ALPHABET[label]
+    
+    def get_label(self):
+        # extracting frames
+        (rval, im) = self.video.read()
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  # grayscale
+        # crop down to a 224x224 square in the center of the webcam
+        #cv2.rectangle(im, (208, 128), (432, 352), (0, 0, 255), 2)
+        cv2.rectangle(im, (208, 128), (656, 576), (0, 0, 255), 2)
+        cropped = im[208:432, 128:352]
+        predictions = self.predict(cropped)
+        # print(predictions)
+        predicted = torch.softmax(predictions,dim=1)
+        _,predicted = torch.max(predicted, 1)
+        label = predicted.data.cpu().numpy()[0] 
+
+        return ALPHABET[label]
