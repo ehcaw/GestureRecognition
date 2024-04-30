b8daa5c7b84278a7e39e5022050e0cb7e24c5cf1
gaelgil <gaelgilbello27@gmail.com>
2024-04-29 20:51:36 -0700

    added comments

--- app.py
+++ app.py
@@ -22,7 +22,11 @@ def gen_frames(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         #return label
+
 def gen_labels(camera):
+    """
+    function to get labels
+    """
     while True:
         label = camera.get_label()
         print('label generated')
--- camera.py
+++ camera.py
@@ -2,16 +2,21 @@ import cv2
 import torch
 from MLModel.model_train import SignLanguageCNN
 
+# define the alphabet. This will be used to turn numerical outputs into labels
 ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 class VideoCamera(object):
     def __init__(self):
-        self.model = SignLanguageCNN()
-        #checkpoint = torch.load("./MLModel/Model_test.pt")
-        checkpoint = torch.load("./MLModel/Model_test.pt", map_location=torch.device('cpu'))
-        self.model.load_state_dict(checkpoint["state_dict"])
+        # load model class
+        self.model = SignLanguageCNN() 
+        # load the trained model
+        checkpoint = torch.load("./MLModel/Model_test.pt", map_location=torch.device('cpu')) 
+        self.model.load_state_dict(checkpoint["state_dict"]) 
+         # set model to eval mode
         self.model.eval()
+         # use gpu if available
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # use opencv to get video from camera
         self.video = cv2.VideoCapture(0)
 
     def __del__(self):
@@ -20,49 +25,47 @@ class VideoCamera(object):
 
 
     def predict(self, frame):
-        resized_frame = cv2.resize(frame, (28, 28))  # resize
+        """
+        function to predict from SignLanguageCNN()
+        """
+        # resize frame
+        resized_frame = cv2.resize(frame, (28, 28))
         #resize back up to 224x224 converting all values to be between 0 and 1
         resized_frame = cv2.resize(resized_frame, (224, 224)) / 255.0
         # finally convert to 4D tensors which then get imput into the model
         tensor_frame = torch.FloatTensor(resized_frame).unsqueeze(0).unsqueeze(0).to(self.device)
-
         self.model.to(self.device)
-
+        # pass frame to model to get output
         with torch.no_grad():
             output=self.model(tensor_frame.to("cuda") if torch.cuda.is_available() else tensor_frame.to("cpu"))
-        
-        return output
+        return output # return output
     
     def get_frame(self):
+        """
+        function to get a frame from the video camera and return it to be used
+        in app.py
+        """
         # extracting frames
         (rval, im) = self.video.read()
         im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  # grayscale
         # crop down to a 224x224 square in the center of the webcam
-        #cv2.rectangle(im, (208, 128), (432, 352), (0, 0, 255), 2)
-        cv2.rectangle(im, (208, 128), (656, 576), (0, 0, 255), 2)
-        cropped = im[208:432, 128:352]
-        predictions = self.predict(cropped)
-        # print(predictions)
-        predicted = torch.softmax(predictions,dim=1)
-        _,predicted = torch.max(predicted, 1)
-        label = predicted.data.cpu().numpy()[0] 
+        cv2.rectangle(im, (208, 128), (656, 576), (0, 0, 255), 2) 
+        cropped = im[208:432, 128:352] # crop the image
+        ret, jpeg = cv2.imencode('.jpg', im) # change frame type
+        return jpeg.tobytes() # return frame 
 
 
-        ret, jpeg = cv2.imencode('.jpg', im)
-        return jpeg.tobytes(), ALPHABET[label]
-    
     def get_label(self):
+        """
+        function to get a labels for an image
+        """
         # extracting frames
         (rval, im) = self.video.read()
         im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  # grayscale
-        # crop down to a 224x224 square in the center of the webcam
-        #cv2.rectangle(im, (208, 128), (432, 352), (0, 0, 255), 2)
         cv2.rectangle(im, (208, 128), (656, 576), (0, 0, 255), 2)
-        cropped = im[208:432, 128:352]
-        predictions = self.predict(cropped)
-        # print(predictions)
-        predicted = torch.softmax(predictions,dim=1)
-        _,predicted = torch.max(predicted, 1)
-        label = predicted.data.cpu().numpy()[0] 
-
-        return ALPHABET[label]
+        cropped = im[208:432, 128:352] # crop the frame from the camera
+        predictions = self.predict(cropped) # predict from a cropped frame
+        predicted = torch.softmax(predictions,dim=1) # get predictions vector
+        _,predicted = torch.max(predicted, 1) # get the max value predicted
+        label = predicted.data.cpu().numpy()[0] # get the numerical prediction
+        return ALPHABET[label] # return the letter label
